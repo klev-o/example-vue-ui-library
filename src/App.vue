e673670ac60db7002503e287c2f487de9f54af44
Konstantin <levachin@luckandroll.com>
2021-09-15 15:46:58 +0300

    Added checkbox custom component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
-<template>
-  <div id="app" class="section">
-    <div class="container">
-      App
-    </div>
-  </div>
-</template>
-
 <script>
+import MCheckbox from "./components/MCheckbox";
 
 export default {
   name: 'App',
   components: {
-
+    MCheckbox
+  },
+  data() {
+    return {
+      form: {
+        rememberMe: true
+      }
+    }
+  },
+  methods : {
+    // onChange(event) {
+    //   console.log(event)
+    //   this.form.rememberMe = event.target.checked
+    // }
   }
 }
 </script>
 
+<template>
+  <div id="app" class="section">
+    <div class="container">
+      <MCheckbox v-model="form.rememberMe" />
+    </div>
+  </div>
+</template>
---
+++ src/components/MCheckbox.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    props: {
+      value: {
+        type: Boolean
+      }
+    }
+  }
+</script>
+
+<template>
+  <label class="checkbox">
+    <input :checked="value" @input="$emit('input', $event.target.checked)" type="checkbox">
+    Remember me
+    <span class="has-text-danger">
+      {{value ? 'checked' : 'not checked'}}
+    </span>
+  </label>
+</template>
